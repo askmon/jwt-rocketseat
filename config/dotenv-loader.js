cbc10f0f6133c11f3b6ba903929b97ceb2a3973e
Rodrigo Botti <rodrigo.botti@gmail.com>
2019-11-20 14:17:03 -0300

    Bugfix: conditional dotenv configuration - skipping in production environments

--- config/dotenv-loader.js
+++ config/dotenv-loader.js
@@ -4,13 +4,9 @@ const DEV_ENVIRONMENTS = ['dev', 'development', 'local', 'debug']
 const TEST_ENVIRONMENTS = ['test']
 const DOTENV_ENVIRONMENTS = [...DEV_ENVIRONMENTS, ...TEST_ENVIRONMENTS]
 
-const load = nodeEnv => {
+const loadDotEnv = nodeEnv => {
   const dotenv = require('dotenv')
 
-  if (!DOTENV_ENVIRONMENTS.includes(nodeEnv)) {
-    console.info('Skipping dotenv configuration')
-    return
-  }
   if (DEV_ENVIRONMENTS.includes(nodeEnv)) {
     return dotenv.config()
   }
@@ -19,6 +15,14 @@ const load = nodeEnv => {
       path: join(__dirname, 'test.env'),
     })
   }
+}
+
+const load = nodeEnv => {
+  if (!DOTENV_ENVIRONMENTS.includes(nodeEnv)) {
+    console.info('Skipping dotenv configuration')
+    return
+  }
+  loadDotEnv(nodeEnv)
 }
 
 module.exports = {

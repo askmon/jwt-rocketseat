624dfcc3695d9d1d01ec5e67756a3b12bdb2ac4f
André <andre.spanguero@gmail.com>
2019-11-20 18:36:18 -0300

    Improvement: JWT security

--- config/crypto.js
+++ config/crypto.js
@@ -2,5 +2,8 @@ module.exports = {
   hashSaltRounds: 10,
   jwt: {
     secret: process.env.JWT_SECRET,
+    duration: process.env.JWT_DURATION || '1h',
+    privateKey: process.env.JWT_PRIVATE_KEY,
+    publicKey: process.env.JWT_PUBLIC_KEY,
   },
 }
--- src/services/token.js
+++ src/services/token.js
@@ -1,13 +1,21 @@
 const jwt = require('jsonwebtoken')
 const { crypto: config } = require('../../config')
 
+const signOptions = {
+  algorithm: 'RS256',
+  expiresIn: config.jwt.duration,
+}
+
 const sign = payload =>
-  jwt.sign(payload, config.jwt.secret)
+  jwt.sign(payload,
+    config.jwt.privateKey,
+    signOptions
+  )
 
 const verify = token => new Promise((resolve, reject) =>
   jwt.verify(
     token,
-    config.jwt.secret,
+    config.jwt.publicKey,
     (error, data) => error ? reject(error) : resolve(data)
   )
 )

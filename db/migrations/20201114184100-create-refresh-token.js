ba16f5160a3534fec67097e5fa7466516aa5f083
André Kanayama <andre.spanguero@gmail.com>
2020-11-14 18:48:15 -0300

    Create 20201114184100-create-refresh-token.js

---
+++ db/migrations/20201114184100-create-refresh-token.js
@@ -0,0 +1,36 @@
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+      return queryInterface.createTable('tokens', {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        user_id: {
+          allowNull: false,
+          type: Sequelize.STRING,
+        },
+        token: {
+          allowNull: false,
+          type: Sequelize.STRING,
+          unique: true,
+        },
+        valid: {
+          allowNull: false,
+          type: Sequelize.BOOLEAN,
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+      })
+    },
+    down: (queryInterface, Sequelize) => {
+      return queryInterface.dropTable('tokens')
+    },
+  }
